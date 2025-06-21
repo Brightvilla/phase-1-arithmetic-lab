514b106a693e2ccc4664c2ce45c50041990b92ed
Brightvilla <166039620+Brightvilla@users.noreply.github.com>
2025-06-21 18:20:00 +0300

    arithmeticlab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+//Multiply ; equation whose result equals to 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+//Random ;generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+//Modulus; equation that calculates a  remainder equal to 4
+const  num3 = 16;
+const num4= 12;
+const mod = num3 % num4;
+
+//Maximum; function retuns 20 as the highest number in the set
+const max = Math.max(1,5,10,20);
